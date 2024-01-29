fe305b5ed005844e59600f97a5dfe2d763c24897
PrashantB-307 <pb28.rkt@gmail.com>
2024-01-29 20:26:04 +0530

    Use Effect

---
+++ 06.React_TypeScript/4-useEffect/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+// import Counter from "./components/Counter";
+import UserList from "./modules/components/UserList";
+
+
+
+const App: React.FC = () => {
+
+  return (
+    <>
+        {/* <Counter/> */}
+        <UserList/>
+
+    </>
+  );
+};
+
+export default App;
